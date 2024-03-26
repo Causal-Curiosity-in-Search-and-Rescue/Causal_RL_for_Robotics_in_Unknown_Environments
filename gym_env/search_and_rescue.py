added scaling reward, added wandb table log for summary, added time taken log, added fixes

gym_env/search_and_rescue.py
@@ -206,6 +206,7 @@ class SearchAndRescueEnv(gym.Env):
     def calculate_reward(self,next_pos,next_cell_code) :
         reward = 0
         done = False
+        goal_reached = False
         
         if next_cell_code in [self.wall_code,self.room_code]: # penalize for collision with wall and room
             reward -= 1
@@ -214,10 +215,15 @@ class SearchAndRescueEnv(gym.Env):
         if self.robot_movement_state[tuple(next_pos)] == 1: # reward for new visits - promotes exploration
             reward += 1
         
-        if next_cell_code == self.goal_code: # reward for reaching goal higher
-            reward += 10
+        if next_cell_code == self.goal_code:  # reward for reaching goal
+            base_reward = 10
+            remaining_steps_ratio = (self.max_steps - self.current_step) / self.max_steps
+            additional_reward = remaining_steps_ratio * 10
+            reward += min(base_reward + additional_reward, 20)
             logging.info(f"[GOAL] Goal Reached @ {self.episode_count}")
+            goal_reached = True
             done = True
+            
 
         # goal_pos = self.get_goal_position()
         # max_dist = self.get_maximum_distance()
@@ -228,7 +234,7 @@ class SearchAndRescueEnv(gym.Env):
         if self.current_step >= self.max_steps:
             done = True
         
-        return reward,done
+        return reward,done,goal_reached
     
     def log_interactions(self): # will log interactions per episode
         wandb.log({"immovable_interactions":self.cumulative_immovable_interactions})
@@ -239,19 +245,29 @@ class SearchAndRescueEnv(gym.Env):
         self.robot_pos = self.get_robot_position()
         next_pos = self.get_fov_cell(self.robot_pos, action)
         cell_code = self.get_fov_info(next_pos)
-        reward,done = self.calculate_reward(next_pos,cell_code)
+        reward,done,goal_reached = self.calculate_reward(next_pos,cell_code)
         self.translate_action(action,self.robot_pos,next_pos,cell_code)
         self.cumulative_reward += reward        
         wandb.log({"episode":self.episode_count,"step":self.current_step})
         wandb.log({"step":self.current_step,"reward":reward})
+        info = {
+            "goal_reached":goal_reached,
+            "episode_count": self.episode_count,
+            "current_step":self.current_step,
+            "cumulative_reward":self.cumulative_reward,
+            "cumulative_interactions":sum([self.cumulative_movable_interactions,self.cumulative_immovable_interactions]),
+            "movable_interactions":self.cumulative_movable_interactions,
+            "non_movable_interactions":self.cumulative_immovable_interactions,
+            "goal_reward":reward
+        }
+        logging.info(f"-EPISODE:{self.episode_count} @ STEP:{self.current_step}- Reward : {reward}")
         if done:
             self.log_interactions()
             wandb.log({"episode":self.episode_count,"cummulative_reward":self.cumulative_reward})
             self.episode_count += 1
         
         self.current_step += 1
-        logging.info(f"-EPISODE:{self.episode_count} @ STEP:{self.current_step}- Reward : {reward}")
-        return self.state, reward, done, {}
+        return self.state, reward, done, info
 
     def normalize_array(self,array):
         if np.max(array) == np.min(array):

train.py
@@ -5,6 +5,7 @@ from stable_baselines3 import PPO,A2C
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
+import time
 import wandb
 import numpy as np 
 import gym 
@@ -34,6 +35,24 @@ def make_env():
     env = gym.wrappers.RecordEpisodeStatistics(env)  # record stats such as returns
     return env
 
+def log_to_wandb(goal_reached,episode_count, current_step, cumulative_reward, cumulative_interactions,movable_interactions,non_movable_interactions, goal_reward,time_taken):
+    if 'summary_table' not in wandb.run.summary:
+        columns = ["goal_reached", "episode_count", "current_step","cumulative_reward", "cumulative_interactions", "movable_interactions","non_movable_interactions","goal_reward","time_taken"]
+        wandb.run.summary['results_table'] = wandb.Table(columns=columns)
+   
+    wandb.run.summary['results_table'].add_data(goal_reached,episode_count, current_step, cumulative_reward, cumulative_interactions,movable_interactions,non_movable_interactions, goal_reward,time_taken)
+    wandb.log({
+        "goal_reached":goal_reached,
+        "episode_count": episode_count,
+        "current_step":current_step,
+        "cumulative_reward":cumulative_reward,
+        "cumulative_interactions":cumulative_interactions,
+        "movable_interactions":movable_interactions,
+        "non_movable_interactions":non_movable_interactions,
+        "goal_reward":reward,
+        "time_taken":time_taken
+    })
+
 config = {
     "model": CONFIG['algorithm'],
     "policy":CONFIG['policy'],
@@ -62,27 +81,45 @@ best_episode = 0
 obs = env.reset()  
 episode_count = 0  
 sum_rewards = 0  
+start_timer = time.time()
 
 for step in range(total_timesteps):
     action, _ = model.predict(obs)
     obs, reward, done, info = env.step(action)
-    # sum_rewards += reward  
 
     if done:
+        time_taken_for_episode = time.time() - start_timer
         episode_rewards.append(sum_rewards)  
-        model.save(os.path.join(log_dir, f'final_model_{episode_count}.zip'))  
-        # if sum_rewards > best_mean_reward:
-        #     best_mean_reward = sum_rewards  
-        #     best_episode = episode_count  
-            
-        #     os.rename(
-        #         os.path.join(log_dir, f'final_model_{episode_count}.zip'),
-        #         os.path.join(log_dir, 'best_model.zip')
-        #     )
-
+        model.save(os.path.join(log_dir, f'final_model_{episode_count}.zip')) 
+        if step >= CONFIG['environment']['max_steps'] - 1:
+            log_to_wandb(
+                info["goal_reached"],
+                info["episode_count"],
+                info["current_step"],
+                info["cumulative_reward"],
+                info["cumulative_interactions"],
+                info["movable_interactions"],
+                info["non_movable_interactions"],
+                info["goal_reward"],
+                time_taken_for_episode
+            )
+        elif info['goal_reached']:
+            log_to_wandb(
+                info["goal_reached"],
+                info["episode_count"],
+                info["current_step"],
+                info["cumulative_reward"],
+                info["cumulative_interactions"],
+                info["movable_interactions"],
+                info["non_movable_interactions"],
+                info["goal_reward"],
+                time_taken_for_episode
+            )
         episode_count += 1  
         # sum_rewards = 0  
         obs = env.reset()  
+        start_timer = time.time()
+        
 
 logging.info(f"Total episodes: {episode_count}")
 logging.info(f"Average reward: {np.mean(episode_rewards)}")